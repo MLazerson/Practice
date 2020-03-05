aaa4b4177f89d6ed0695980e6eb009b7882f2c97
mlazerson <mlazerson@csu.fullerton.edu>
2020-03-05 11:12:07 -0800

    vector practice documentation

--- vectorEx.cpp
+++ vectorEx.cpp
@@ -1,26 +1,26 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+// read file
 void readFile(std::vector<double> & vec, std::string fname) {
   double temp = 0;
     // open file
     std::ifstream infile;
     infile.open(fname);
-
+    // read in as doubles
     while (infile >> temp){
       vec.push_back(temp);
     }
+    // close file
     infile.close();
 }
-
+// print
 void printNumbers(const std::vector <double> &vec) {
-
-for(int i = 0; i < vec.size();i++) {
-  std::cout << vec.at(i) << std::endl;
+ for(int i = 0; i < vec.size();i++) {
+   std::cout << vec.at(i) << std::endl;
+ }
 }
-
-}
-
+// main
 int main() {
   std::vector<double> myVector;
   std::string fileName = "numbers.txt";
